dc7e9374ccc5406cba8297a3f6bd5597da082846
William Ong <wlo12489@gmail.com>
2026-05-03 02:48:17 -0700

    refined cognitive tasks code

--- src/utils/cognitive_tasks.py
+++ src/utils/cognitive_tasks.py
@@ -10,55 +10,65 @@ import matplotlib.pyplot as plt
 import os
 
 import utils.visualization_utils as vis
+from utils.common_utils import normalize
+
 
 pi = np.pi
 SAVE_DIR = "./graphs/"
 
 
 class Task:
-    def __init__(
-        self,
-        T: int = 200,
-    ):
-        """ Parent class for all tasks. """
+    """
+    Parent class for all tasks. 
+    """
+    def __init__(self, T: int = 200):
+        """
+        Args:
+            T: Time size.
+        """
         self.T = T
     
     def gen_fixation(self, start, end=None):
-        """ Fixation is 1. except for the period (start, end). """
+        """
+        Fixation is 1. except for the period (start, end).
+        """
         fix = np.ones((self.T, 1))
-        fix[start: end] = 0. # after go cue, fixation is off
+        fix[start: end] = 0.0 # after go cue, fixation is off
         return fix
     
     def gen_stimulus(self, itvl, start, end=None, amp=1.):
-        """ Generate stimulus during the period (start, end), drawn from the interval ``itvl`` with amplitude ``amp``. """
+        """
+        Generate stimulus during the period (start, end), drawn from the interval ``itvl`` with amplitude ``amp``.
+        """
         stim = np.zeros((self.T, 1))
         
-        # draw random angle for stimulus
-        # if itvl is scalar, it is no longer randomly drawn
-        if isinstance(itvl, float): 
-            theta = itvl 
-        else: 
-            theta = np.random.uniform(*itvl)
+        # Draw random angle for stimulus, if itvl is scalar, it is no longer randomly drawn
+        if isinstance(itvl, float): theta = itvl 
+        else: theta = np.random.uniform(*itvl)
             
+        # Normalize the stimulus angle
         stim[start: end, 0] = normalize(theta)
         return stim, theta, amp
     
     def gen_response(self, theta, start, end=None):
-        """ Generate response ``theta`` during the period (start, end). """
+        """
+        Generate response ``theta`` during the period (start, end).
+        """
         resp = np.zeros((self.T, 1))
         resp[start: end, 0] = normalize(theta)
         return resp
     
     def gen_single_trial(self):
-        """ To be implemented in children classes. """
-        raise NotImplementedError
+        raise NotImplementedError('gen_single_trial function must be implemented in subclass!')
         
     def draw(self, save=False, figname="task", n_batches=4):
+        # Plot the task, each column is a batch
         fig, axs = plt.subplots(5, n_batches, figsize=(8, 8), sharex=True, sharey=True)
         colors = ["k", "r", "b", "g", "k"]
         
         def round2(num): return str(round(num, 2))
         
+        # Plot each batch
         for b in range(n_batches):
             fix, (stim1, amp1), (stim2, amp2), resp, sacc = self.gen_single_trial()
             axs[0, b].plot(fix.squeeze(), color="k")
@@ -82,24 +92,21 @@ class Task:
         plt.tight_layout()
         if save: vis.savefig(os.path.join(SAVE_DIR, f"{figname}.png"), clear=True, close=True)
     
+
 class RTGo(Task):
-    def __init__(
-        self,
-        hparams: dict,
-        T: int = 200,
-    ):
+    """
+    Fixation: fixation cue never goes off.
+    Stimulus: stimulus occurs randomly at either channel 1 or 2.
+    Response: should return stimulus direction, or the one opposite of it. [see hparams/anti]
+    Saccade: should respond immediately after stimulus arrives.
+    """
+    def __init__(self, hparams: dict, T: int = 200):
         """
-        Task description:
-            Fixation: fixation cue never goes off.
-            Stimulus: stimulus occurs randomly at either channel 1 or 2.
-            Response: should return stimulus direction, or the one opposite of it. [see hparams/anti]
-            Saccade: should respond immediately after stimulus arrives.
-        
         Args:
             hparams:
                 Hyperparameters for this task; this dictionary contains the following (key, value) pairs:
                 - anti: bool, whether to respond to the opposite direction of the stimulus
-            T: time size
+            T: Time size.
         """
         super().__init__(T)
         self.hps = hparams     
@@ -121,73 +128,67 @@ class RTGo(Task):
         else:
             return fix, (null, 1.), (stim, 1.), resp, sacc
             
+
 class DlyGo(Task):
-    def __init__(
-        self,
-        hparams: dict,
-        T: int = 200,
-    ):
+    """
+    Fixation: fixation cue goes off after a stimulus ends + delay period.
+    Stimulus: stimulus occurs randomly at either channel 1 or 2.
+    Response: should return stimulus direction, or the one opposite of it. [see hparams/anti]
+    Saccade: should respond after fixation cue goes off.
+    """
+    def __init__(self, hparams: dict, T: int = 200):
         """
-        Task description:
-            Fixation: fixation cue goes off after a stimulus ends + delay period.
-            Stimulus: stimulus occurs randomly at either channel 1 or 2.
-            Response: should return stimulus direction, or the one opposite of it. [see hparams/anti]
-            Saccade: should respond after fixation cue goes off.
-        
         Args:
             hparams:
                 Hyperparameters for this task; this dictionary contains the following (key, value) pairs:
-                - anti: bool
-            T: time size
+                - anti: bool, whether to respond to the opposite direction of the stimulus
+            T: Time size.
         """
         super().__init__(T)
         self.hps = hparams
         
     def gen_single_trial(self):
-        start = np.random.randint(30, 50)
-        stim_dur = np.random.randint(30, 50)
-        stim, theta, _ = self.gen_stimulus([-pi, pi], start, start + stim_dur)
-        null = np.zeros_like(stim)
+        start = np.random.randint(30, 50) # start time of stimulus
+        stim_dur = np.random.randint(30, 50) # duration time of stimulus
+        stim, theta, _ = self.gen_stimulus([-pi, pi], start, start + stim_dur) # generate target stimulus
+        null = np.zeros_like(stim) # generate "null" stimulus
 
         dly_dur = np.random.randint(30, 50) # delay period after stimulus ends
-        go_time = start + stim_dur + dly_dur # go time at arrives at t=150
-        fix = self.gen_fixation(go_time)
+        go_time = start + stim_dur + dly_dur  # random go cue time (depends on start, stim_dur, dly_dur)
+        fix = self.gen_fixation(go_time) # fixation cue goes off after stimulus ends + delay period
 
-        psi = np.pi + theta if self.hps["anti"] else theta
-        resp = self.gen_response(psi, go_time)
-        sacc = self.gen_fixation(go_time)
+        psi = np.pi + theta if self.hps["anti"] else theta # if response is opposite
+        resp = self.gen_response(psi, go_time) # generate response
+        sacc = self.gen_fixation(go_time) # saccade after fixation cue goes off
         
-        if np.random.randint(2): # determine target stimulus channel
+        if np.random.randint(2): # determine target stimulus channel, 50% chance to choose target stimulus channel
             return fix, (stim, 1.), (null, 1.), resp, sacc
         else:
             return fix, (null, 1.), (stim, 1.), resp, sacc
             
+
 class CtxDM(Task):
-    def __init__(
-        self,
-        hparams: dict,
-        T: int = 200,
-    ):
+    """
+    Fixation: fixation cue goes off after a stimulus ends + delay period.
+    Stimulus: stimulus occurs at both channels, could be offset from one another. [see hparams/offset]
+    Response: should return based on task type. [see hparams/to_choose]
+    Saccade: should respond after fixation cue. 
+    """
+    def __init__(self, hparams: dict, T: int = 200):
         """
-        Task description:
-            Fixation: fixation cue goes off after a stimulus ends + delay period.
-            Stimulus: stimulus occurs at both channels, could be offset from one another. [see hparams/offset]
-            Response: should return based on task type. [see hparams/to_choose]
-            Saccade: should respond after fixation cue. 
-            
         Args:
             hparams:
                 Hyperparameters for this task; this dictionary contains the following (key, value) pairs:
                 - to_choose: int, whether to choose stimulus 1 or 2, or the stronger one (3)
                 - offset: bool, whether to offset the stimulus start time
-            T: time size
+            T: Time size.
         """
         super().__init__(T)
         self.hps = hparams
         
     def gen_single_trial(self):
-        start = np.random.randint(30, 50)
-        stim_dur = np.random.randint(30, 50)
+        start = np.random.randint(30, 50) # start time of stimulus
+        stim_dur = np.random.randint(30, 50) # duration time of stimulus
         offset = np.random.randint(10, 20) * int(np.sign(np.random.randn())) if self.hps["offset"] else 0
         amp1, amp2 = np.random.uniform(0.5, 1.5), np.random.uniform(0.5, 1.5) # stimulus amplitude
         
@@ -202,11 +203,13 @@ class CtxDM(Task):
             amp=amp2,
         )
 
+        # Generate delay period
         dly_dur = np.random.randint(30, 50)
-        go_time = start + stim_dur + max(0, offset) + dly_dur # go cue at most arrives at t=170
+        go_time = start + stim_dur + max(0, offset) + dly_dur  # go cue after stimulus + delay (timing is random)
         fix = self.gen_fixation(go_time)
         sacc = self.gen_fixation(go_time)
 
+        # Generate response based on task type
         if self.hps["to_choose"] == 1:
             resp = self.gen_response(theta1, go_time)
         elif self.hps["to_choose"] == 2:
@@ -219,29 +222,27 @@ class CtxDM(Task):
             
         return fix, (stim1, amp1), (stim2, amp2), resp, sacc
     
+
 class Match(Task):
-    def __init__(
-        self,
-        hparams: dict,
-        T: int = 200,
-    ):
+    """
+    Fixation: fixation cue goes off after second stimulus ends + delay period.
+    Stimulus: stimulus occurs at both channels, is offset from one another
+    Response: should return stimulus2 direction (or opposite). [see hparams/anti]
+    Saccade: should respond after fixation cue goes off if the two stimuli match (based on condition), else fixate. [see hparams/cond]
+    """
+    def __init__(self, hparams: dict, T: int = 200):
         """
-        Task description:
-            Fixation: fixation cue goes off after second stimulus ends + delay period.
-            Stimulus: stimulus occurs at both channels, is offset from one another
-            Response: should return stimulus2 direction (or opposite). [see hparams/anti]
-            Saccade: should respond after fixation cue goes off if the two stimuli match (based on condition), else fixate. [see hparams/cond]
-            
         Args:
             hparams:
                 Hyperparameters for this task; this dictionary contains the following (key, value) pairs:
                 - anti: bool, to choose opposite direction or not
                 - cond: str, "point" (if stim1==stim2) or "category" (if they are in same category)
-            T: time size
+            T: Time size.
         """
         super().__init__(T)
         self.hps = hparams
         
+        # Determine the condition for matching
         if self.hps["cond"] == "point":
             self.same = self.same_value
         elif self.hps["cond"] == "category":
@@ -250,18 +251,21 @@ class Match(Task):
             raise ValueError
         
     def gen_single_trial(self):
-        start = np.random.randint(30, 50)
-        stim_dur = 30
-        offset = np.random.randint(10, 20) * int(np.sign(np.random.randn()))
-        stim1, theta1, _ = self.gen_stimulus([-pi, pi], start, start + stim_dur)
+        start = np.random.randint(30, 50) # start time of stimulus
+        stim_dur = 30 # duration time of stimulus
+        offset = np.random.randint(10, 20) * int(np.sign(np.random.randn())) # offset of stimulus
+        stim1, theta1, _ = self.gen_stimulus([-pi, pi], start, start + stim_dur) # generate target stimulus 1
         
+        # Generate target stimulus 2
         theta2 = theta1 if np.random.randint(2) else theta1 - pi # if theta1 matches theta2 or not
         stim2, _, _ = self.gen_stimulus(theta2, start + offset, start + stim_dur + offset)
         
+        # Generate delay period
         dly_dur = np.random.randint(30, 50)
-        go_time = start + stim_dur + max(offset, 0) + dly_dur # at most t=150
+        go_time = start + stim_dur + max(offset, 0) + dly_dur  # go cue after stimuli + delay (timing is random)
         fix = self.gen_fixation(go_time)
         
+        # Generate response based on condition
         if self.same(theta1, theta2):
             sacc = self.gen_fixation(go_time)
             psi = theta2 if not self.hps["anti"] else pi + theta2 # whether to respond opposite or not
@@ -275,6 +279,9 @@ class Match(Task):
     
     @staticmethod
     def same_category(ang1, ang2):
+        """
+        Check if the two angles are in the same category.
+        """
         is_pos1 = normalize(ang1) > 0
         is_pos2 = normalize(ang2) > 0
         return not np.logical_xor(is_pos1, is_pos2)
@@ -283,9 +290,8 @@ class Match(Task):
     def same_value(ang1, ang2):
         return ang1 == ang2
     
-# ====== Task map ===== #
-# key is the task name [delay (if applicable) + task type + anti (if applicable) + stimulus to choose] 
-# value is task class, hparam dict
+
+# Task Map, where key is the task name [delay (if applicable) + task type + anti (if applicable) + stimulus to choose] 
 task_map = {
     "rt_go": [RTGo, {"anti": False}],
     "rt_go_anti": [RTGo, {"anti": True}],
